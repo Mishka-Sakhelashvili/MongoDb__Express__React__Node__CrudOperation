4645c833d7f8c35897fe16f40c3e911129a8af82
Mikheil Sakhelashvili <mishka.sakhelashvili@gmail.com>
2020-11-26 12:55:48 +0400

    add

---
+++ Server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+// Routes
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+// MongoDb
+// mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false
+const CONNECTION_URL = "mongodb://localhost:27017/mern";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
